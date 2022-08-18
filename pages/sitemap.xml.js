b5acf2346ff019237940feda5bf27dc5f77f1a48
bajwaghazanfar <67128537+bajwaghazanfar@users.noreply.github.com>
2022-08-18 19:52:23 +0100

    added new sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,31 +1,31 @@
 import React from "react";
 import * as fs from "fs";
-
 const Sitemap = () => {
   return null;
 };
 
 export const getServerSideProps = async ({ res }) => {
-  
-  const BASE_URL = 'https://www.obsidianwebdevelopment.com'; //This is where you will define your base url. You can also use the default dev url http://localhost:3000
+  const BASE_URL = "https://www.obsidianwebdevelopment.com";
 
   const staticPaths = fs
     .readdirSync("pages")
     .filter((staticPage) => {
       return ![
-        "sitemap.xml.jsx",
-        "404.js",
+        "api",
+        "product",
         "_app.js",
         "_document.js",
-        "api"
+        "404.js",
+        "sitemap.xml.js",
       ].includes(staticPage);
     })
-  .map((staticPagePath) => {
-    return `${BASE_URL}/${staticPagePath.replace('.js', '')}`;
-  });
+    .map((staticPagePath) => {
+      return `${BASE_URL}/${staticPagePath}`;
+    });
 
+  const dynamicPaths = [`${BASE_URL}/product/1`, `${BASE_URL}/product/2`];
 
-  const allPaths = [...staticPaths,];
+  const allPaths = [...staticPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -34,10 +34,13 @@ export const getServerSideProps = async ({ res }) => {
           return `
             <url>
               <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
             </url>
           `;
         })
-      .join("")}
+        .join("")}
     </urlset>
   `;
 
